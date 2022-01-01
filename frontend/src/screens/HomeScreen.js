54b0ba877a2604f1360e61ef7445cbc7aa6201f3
Apangshu-2021 <80478098+Apangshu-2021@users.noreply.github.com>
2022-01-02 02:17:52 +0530

    third commit

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,33 +1,38 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useDispatch, useSelector } from 'react-redux'
+import { listProducts } from '../actions/productActions'
+import Loader from '../components/Loader.js'
+import Message from '../components/Message.js'
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([])
-  const host = 'http://localhost:5000'
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const res = await axios.get(`${host}/api/products`)
+  const dispatch = useDispatch()
+  const productList = useSelector((state) => state.productList)
+  const { loading, products, error } = productList
 
-      setProducts(res.data)
-    }
-    fetchProducts()
-  }, [])
+  useEffect(() => {
+    dispatch(listProducts())
+  }, [dispatch])
 
   return (
     <>
       <main className='mt-3'>
         <Container>
           <h1>Latest Products</h1>
-          <Row>
-            {products.map((product) => (
-              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product} />
-              </Col>
-            ))}
-          </Row>
+          {loading ? (
+            <Loader />
+          ) : error ? (
+            <Message variant='danger' message={error} />
+          ) : (
+            <Row>
+              {products.map((product) => (
+                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                  <Product product={product} />
+                </Col>
+              ))}
+            </Row>
+          )}
         </Container>
       </main>
     </>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -8,25 +8,24 @@ import {
   Button,
   Card,
 } from 'react-bootstrap'
-import { useParams, Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import Rating from '../components/Rating'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { listProductDetails } from '../actions/productActions'
+import Loader from '../components/Loader.js'
+import Message from '../components/Message.js'
 
 const ProductScreen = () => {
+  const dispatch = useDispatch()
   const { id } = useParams()
+  const productDetails = useSelector((state) => state.productDetails)
 
-  const [product, setProduct] = useState({})
-  const host = 'http://localhost:5000'
+  const { loading, product, error } = productDetails
 
   useEffect(() => {
-    const fetchProduct = async () => {
-      const res = await axios.get(`${host}/api/products/${id}`)
-
-      setProduct(res.data)
-    }
-    fetchProduct()
-  }, [])
+    dispatch(listProductDetails(id))
+  }, [dispatch, id])
 
   return (
     <>
@@ -35,56 +34,64 @@ const ProductScreen = () => {
           <Link className='btn btn-light my-3' to='/'>
             Go Back
           </Link>
-          <Row>
-            <Col md={6}>
-              <Image src={product.image} alt={product.name} fluid />
-            </Col>
-            <Col md={3}>
-              <ListGroup variant='flush'>
-                <ListGroup.Item>
-                  <h3>{product.name}</h3>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  <Rating
-                    rating={product.rating}
-                    text={`${product.numReviews} reviews`}
-                  />
-                </ListGroup.Item>
-                <ListGroup.Item>Price:${product.price}</ListGroup.Item>
-                <ListGroup.Item>
-                  Description:{product.description}
-                </ListGroup.Item>
-              </ListGroup>
-            </Col>
-            <Col md={3}>
-              <Card className='ms-5'>
-                {/* className='ms-5' is added because it overlaps the above components at smaller dimensions */}
+          {loading ? (
+            <Loader />
+          ) : error ? (
+            <Message variant='danger'>{error}</Message>
+          ) : (
+            <Row>
+              <Col md={6}>
+                <Image src={product.image} alt={product.name} fluid />
+              </Col>
+              <Col md={3}>
                 <ListGroup variant='flush'>
                   <ListGroup.Item>
-                    <Row>
-                      <Col>Price:</Col>
-                      <Col>
-                        <strong>${product.price}</strong>
-                      </Col>
-                    </Row>
+                    <h3>{product.name}</h3>
                   </ListGroup.Item>
                   <ListGroup.Item>
-                    <Row>
-                      <Col>Status:</Col>
-                      <Col>
-                        {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
-                      </Col>
-                    </Row>
+                    <Rating
+                      rating={product.rating}
+                      text={`${product.numReviews} reviews`}
+                    />
                   </ListGroup.Item>
-                  <ListGroup.Item className='btn-group d-flex' role='group'>
-                    <Button className='btn' type='button'>
-                      Add To Cart
-                    </Button>
+                  <ListGroup.Item>Price:${product.price}</ListGroup.Item>
+                  <ListGroup.Item>
+                    Description:{product.description}
                   </ListGroup.Item>
                 </ListGroup>
-              </Card>
-            </Col>
-          </Row>
+              </Col>
+              <Col md={3}>
+                <Card className='ms-5'>
+                  {/* className='ms-5' is added because it overlaps the above components at smaller dimensions */}
+                  <ListGroup variant='flush'>
+                    <ListGroup.Item>
+                      <Row>
+                        <Col>Price:</Col>
+                        <Col>
+                          <strong>${product.price}</strong>
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      <Row>
+                        <Col>Status:</Col>
+                        <Col>
+                          {product.countInStock > 0
+                            ? 'In Stock'
+                            : 'Out Of Stock'}
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                    <ListGroup.Item className='btn-group d-flex' role='group'>
+                      <Button className='btn' type='button'>
+                        Add To Cart
+                      </Button>
+                    </ListGroup.Item>
+                  </ListGroup>
+                </Card>
+              </Col>
+            </Row>
+          )}
         </Container>
       </main>
     </>
